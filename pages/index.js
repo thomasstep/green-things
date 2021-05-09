200d3cf09c0e8241a898a516dbc6283339b7d727
Thomas Step <tstep916@gmail.com>
2021-05-09 12:07:37 -0500

    Seedling favicon

--- pages/index.js
+++ pages/index.js
@@ -37,6 +37,10 @@ export default function Home() {
       <Head>
         <title>Green Things</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main className="flex flex-col items-center justify-center flex-1 mx-auto text-center">
